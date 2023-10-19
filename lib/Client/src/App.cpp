fc2bbeafb7c3e70d273d5ea68f4466d8d5eb9ddf
Isho Antar <0xisho@proton.me>
2023-10-19 07:55:02 +0000

    Add some assertions for sanity checking

--- lib/Client/src/App.cpp
+++ lib/Client/src/App.cpp
@@ -1,13 +1,18 @@
 #include "Client/App.hpp"
 
+#include "Debug/Assert.hpp"
+
 namespace Gaze::Client {
 	App::App(int /*argc*/, char** /*argv*/)
 	{
+		GAZE_ASSERT(m_IsRunning == false, "The application should not be running yet.");
 	}
 
 	App::~App()
 	{
 		m_IsRunning = false;
+
+		GAZE_ASSERT(m_IsRunning == false, "The application is still running at the end of the destructor. It should not be!");
 	}
 
 	auto App::Run() -> Status
@@ -18,6 +23,7 @@ namespace Gaze::Client {
 
 		m_IsRunning = true;
 
+		GAZE_ASSERT(m_IsRunning == true, "The application is not running upon reaching the main loop.");
 		while (m_IsRunning) {
 			OnUpdate();
 
@@ -31,6 +37,7 @@ namespace Gaze::Client {
 
 		m_IsRunning = false;
 
+		GAZE_ASSERT(m_IsRunning == false, "The application should not be running at this point.");
 		return ret;
 	}
 }
