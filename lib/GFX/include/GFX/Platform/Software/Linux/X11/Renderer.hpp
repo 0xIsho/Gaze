cf05797b9c7eca0e297c0aefbe2354a76c069b35
Isho Antar <0xisho@proton.me>
2023-12-10 08:03:32 +0200

    Mark X11::Renderer::SetColor() "override"

--- lib/GFX/include/GFX/Platform/Software/Linux/X11/Renderer.hpp
+++ lib/GFX/include/GFX/Platform/Software/Linux/X11/Renderer.hpp
@@ -11,7 +11,7 @@ namespace Gaze::GFX::Platform::Software::Linux::X11 {
 		Renderer(Mem::Shared<WM::Window> window);
 		~Renderer();
 
-		auto SetColor(F32 r, F32 g, F32 b, F32 a) -> void;
+		auto SetColor(F32 r, F32 g, F32 b, F32 a) -> void override;
 		auto SetClearColor(F32 r, F32 g, F32 b, F32 a) -> void override;
 		auto Clear() -> void override;
 		auto Render() -> void override;
