88a6e961c429a7cdd4f852486f7438152f21727c
Isho Antar <0xisho@proton.me>
2023-12-12 07:57:05 +0200

    Setup a window

--- demo/Games/Pong/main.cpp
+++ demo/Games/Pong/main.cpp
@@ -2,6 +2,8 @@
 
 #include "Core/Type.hpp"
 
+#include "WM/Window.hpp"
+
 using namespace Gaze;
 
 class MyApp : public Client::App
@@ -13,15 +15,24 @@ private:
 	auto OnInit() -> Status;
 	auto OnUpdate(F64 deltaTime) -> void;
 	auto OnShutdown() -> Status;
+
+private:
+	Mem::Shared<WM::Window> m_Win;
 };
 
 MyApp::MyApp(int argc, char** argv)
 	: App(argc, argv)
+	, m_Win(Mem::MakeShared<WM::Window>("Gaze - Pong", 864, 486))
 {
 }
 
 auto MyApp::OnInit() -> Status
 {
+	m_Win->OnClose([this] {
+		Quit();
+	});
+	m_Win->Show();
+
 	return Status::Success;
 }
 
