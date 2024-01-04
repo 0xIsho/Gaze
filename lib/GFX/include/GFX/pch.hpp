c256fdc7d7a4102ec23680abd5a95b4ca0572fb0
Isho Antar <0xisho@proton.me>
2024-01-05 00:31:03 +0200

    Implement private precompiled-header support for GFX and WM
    "private" meaning user-code doesn't depend on the PCH and can provide
    their own.

---
+++ lib/GFX/include/GFX/pch.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <array>
+#include <cstdio>
+#include <vector>
+#include <stdio.h>
+#include <utility>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+#include <initializer_list>
+
+#include "glad/gl.h"
+#include "KHR/khrplatform.h"
+
+#include <GLFW/glfw3.h>
+
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/geometric.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
---
+++ lib/WM/include/WM/pch.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <string_view>
+
+#include <GLFW/glfw3.h>
