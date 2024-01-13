53ba51ee64301d997fcfadc57aca4acbf4d70251
Isho Antar <0xisho@proton.me>
2024-01-13 09:40:32 +0200

    Document the WM subsystem

--- lib/WM/include/WM/Core.hpp
+++ lib/WM/include/WM/Core.hpp
@@ -1,8 +1,32 @@
 #pragma once
 
 namespace Gaze::WM {
+	/**
+	 * @brief Initialise the Window Manager
+	 *
+	 * This function MUST be called first before using any library functionality
+	 *
+	 * @return true if the library was successfully initialised, false otherwise
+	 */
 	[[nodiscard]] auto Init()          noexcept -> bool;
+
+	/**
+	 * @brief Terminates the Window Manager and releases any associated resources
+	 */
 	              auto Terminate()     noexcept -> void;
+
+	/**
+	 * @brief Check whether the Window Manager is initialised or not
+	 *
+	 * @return true if the WM is initialised, false otherwise
+	 */
 	[[nodiscard]] auto IsInitialized() noexcept -> bool;
+
+	/**
+	 * @brief Dispatch new events received from the underlying windowing system
+	 *
+	 * This function should be called regularly to make sure that windows remain
+	 * responsive.
+	 */
 	              auto PollEvents()    noexcept -> void;
 }
--- lib/WM/include/WM/Window.hpp
+++ lib/WM/include/WM/Window.hpp
@@ -9,35 +9,95 @@
 struct GLFWwindow;
 
 namespace Gaze::WM {
+	/**
+	 * @brief Represents an Operating System window
+	 */
 	class Window
 	{
 	public:
 		using EventCallback = std::function<void(Events::Event&)>;
 
 	public:
+		/**
+		 * Construct a new window object
+		 *
+		 * The Window Manager must be initialised, through a successful call to
+		 * @ref WM::Init(), before a Window can be created.
+		 *
+		 * The new Window is hidden by default, it can be made visible through a
+		 * call to @ref Window::Show().
+		 *
+		 * @param title The window title
+		 * @param width The window width, must be > 0
+		 * @param height The window height, must be > 0
+		 */
 		Window(std::string_view title, int width, int height);
 		Window(const Window&) = delete;
+
+		/**
+		 * @brief Move constructor
+		 */
 		Window(Window&& other)                     noexcept;
+
+		/**
+		 * @brief Destructor - Has a similar effect to @ref Window::Close()
+		 */
 		~Window();
 
 		auto operator=(const Window&) -> Window& = delete;
+
+		/**
+		 * @brief Move assignment operator
+		 */
 		auto operator=(Window&& other)             noexcept -> Window&;
 
+		/**
+		 * @brief Makes the Window visible
+		 *
+		 * @note Windows are hidden by default
+		 */
 		auto Show()                                noexcept -> void;
+
+		/**
+		 * @brief Hides the Window
+		 */
 		auto Hide()                                noexcept -> void;
+
+		/**
+		 * @brief Close the Window and release any associated resources
+		 *
+		 * After calling this function, the Window object does *not* refer to a
+		 * valid system window object and must be discarded.
+		 */
 		auto Close()                               noexcept -> void;
 
+		/**
+		 * @brief Register a callback to be called when events occur
+		 *
+		 * @see WM::PollEvents()
+		 */
 		auto OnEvent(EventCallback callback)       noexcept -> void;
 
+		/**
+		 * @brief Retrieve the Window's width
+		 */
 		[[nodiscard]] auto Width()           const noexcept -> int;
+
+		/**
+		 * @brief Retrieve the Window's height
+		 */
 		[[nodiscard]] auto Height()          const noexcept -> int;
+
+		/**
+		 * @brief Retrieve the underlying, platform-specific window handle
+		 */
 		[[nodiscard]] auto Handle()          const noexcept -> void*;
 
 	private:
 		GLFWwindow* m_Handle = nullptr;
 		std::string m_Title;
-		int m_Width = 0;
-		int m_Height = 0;
+		int         m_Width = 0;
+		int         m_Height = 0;
 
 		EventCallback m_CbEvent = [] (auto&) { };
 	};
