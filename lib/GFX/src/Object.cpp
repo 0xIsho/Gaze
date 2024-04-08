ca3aa2bedbedac84a72dff943e03e9591e5cd06c
Isho Antar <0xisho@proton.me>
2024-04-08 12:52:34 +0200

    Use parameters instead of hardcoded values

--- lib/GFX/src/Object.cpp
+++ lib/GFX/src/Object.cpp
@@ -15,11 +15,10 @@ namespace Gaze::GFX {
 	auto Object::SetPosition(const glm::vec3& pos) -> void
 	{
 		m_Properties.transform[3] = { pos, 1.0F };
-
 	}
 
 	auto Object::Rotate(float angleRadians, const glm::vec3& axis) -> void
 	{
-		m_Properties.transform *= glm::mat4_cast(glm::angleAxis(glm::radians(-90.0F), glm::vec3{ 1.0F, .0F, .0F }));
+		m_Properties.transform *= glm::mat4_cast(glm::angleAxis(glm::radians(angleRadians), axis));
 	}
 }
