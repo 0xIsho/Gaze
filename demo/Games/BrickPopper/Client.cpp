382ee79d442024630b10ad6c7ec744ba4756f51a
Isho Antar <0xisho@proton.me>
2024-04-11 16:34:42 +0200

    Setup and render player paddle
    Fixes: $11i

--- demo/Games/BrickPopper/Client.cpp
+++ demo/Games/BrickPopper/Client.cpp
@@ -6,7 +6,20 @@
 #include "Events/Event.hpp"
 #include "Events/WindowEvent.hpp"
 
+#include "GFX/Renderer.hpp"
+#include "GFX/Object.hpp"
+#include "GFX/Primitives.hpp"
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
 using namespace Gaze;
+
+static constexpr auto kWindowWidth        = 1200;
+static constexpr auto kWindowHeight       = 1024;
+static constexpr auto kPaddleWidth        = 100;
+static constexpr auto kPaddleHeight       = 10;
+static constexpr auto kPaddleBottomOffset = 50;
 
 class BrickPopperClient : public Client::ClientApp
 {
@@ -20,19 +33,37 @@ private:
 	auto OnShutdown() -> Status override;
 	auto OnPacketReceived(U32 sender, Net::Packet packet) -> void override;
 
+	auto RenderPlayers() -> void;
+
 private:
-	WM::Window m_Window;
+	Shared<WM::Window> m_Window;
+	Unique<GFX::Renderer> m_Renderer;
+
+	GFX::Object m_Paddle;
+	glm::vec2 m_PlayerPos;
 };
 
 BrickPopperClient::BrickPopperClient(int argc, char** argv)
 	: ClientApp(argc, argv)
-	, m_Window("Brick Popper", 1200, 1024)
+	, m_Paddle(GFX::Primitives::CreateQuad({}, kPaddleWidth, kPaddleHeight))
+	, m_PlayerPos({ kWindowWidth / 2, kPaddleBottomOffset })
 {
+	m_Paddle.Rotate(glm::radians(-90.0F), glm::vec3{ 1.0F, .0F, .0F });
 }
 
 auto BrickPopperClient::OnInit() -> Status
 {
-	m_Window.OnEvent([this](auto& event) {
+	m_Window = MakeShared<WM::Window>("Brick Popper", kWindowWidth, kWindowHeight);
+	m_Renderer = GFX::CreateRenderer(m_Window);
+
+	if (m_Renderer == nullptr) {
+		Logger().Error("Error creating a renderer.");
+		return Status::Fail;
+	}
+
+	m_Renderer->SetCamera(MakeShared<GFX::OrthographicCamera>(0.F, F32(kWindowWidth), 0.F, F32(kWindowHeight), .1F, 10.F));
+
+	m_Window->OnEvent([this](auto& event) {
 		auto dispatcher = Events::Dispatcher(event);
 
 		dispatcher.Dispatch<Events::WindowClose>([this](auto& event) {
@@ -40,14 +71,24 @@ auto BrickPopperClient::OnInit() -> Status
 		});
 	});
 
-	m_Window.Show();
+	m_Window->Show();
 
 	return Status::Success;
 }
 
 auto BrickPopperClient::OnUpdate(F64 deltaTime) -> void
 {
+	m_Renderer->Clear();
 
+	RenderPlayers();
+
+	m_Renderer->Render();
+}
+
+auto BrickPopperClient::RenderPlayers() -> void
+{
+	m_Paddle.SetPosition({ m_PlayerPos, 0 });
+	m_Renderer->SubmitObject(m_Paddle, GFX::Renderer::PrimitiveMode::Triangles);
 }
 
 auto BrickPopperClient::OnFixedUpdate(F64 deltaTime) -> void
