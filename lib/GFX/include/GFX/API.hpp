b5a1cb45e3c88071a7d57e6d0145d541ab4b1c7b
Isho Antar <0xisho@proton.me>
2024-04-22 19:27:49 +0200

    GFX: Document the public API

--- lib/GFX/include/GFX/API.hpp
+++ lib/GFX/include/GFX/API.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
 namespace Gaze::GFX {
+	/**
+	 * @brief The API to use for rendering.
+	 */
 	enum class API {
 		kOpenGL
 	};
 
+	/**
+	 * @brief Set the graphics API.
+	 */
 	auto SetAPI(API api) noexcept -> void;
+	/**
+	 * @brief Get the graphics API.
+	 */
 	auto GetAPI()        noexcept -> API;
 }
--- lib/GFX/include/GFX/Primitives.hpp
+++ lib/GFX/include/GFX/Primitives.hpp
@@ -20,9 +20,40 @@ namespace Gaze::GFX {
 	[[nodiscard]] auto CreateQuad(glm::vec3 position, F32 width, F32 height) -> Mesh;
 
 	namespace Primitives {
+		/**
+		 * @brief Create an Object representing a point
+		 *
+		 * @param point The position of the point
+		 *
+		 * @return The created Object
+		 */
 		[[nodiscard]] auto CreatePoint(glm::vec3 point) -> Object;
+		/**
+		 * @brief Create an Object representing a line
+		 *
+		 * @param start The start of the line
+		 * @param end The end of the line
+		 *
+		 * @return The created Object
+		 */
 		[[nodiscard]] auto CreateLine(glm::vec3 start, glm::vec3 end) -> Object;
+		/**
+		 * @brief Create an Object representing a triangle
+		 *
+		 * @param points The points of the triangle
+		 *
+		 * @return The created Object
+		 */
 		[[nodiscard]] auto CreateTriangle(std::array<glm::vec3, 3> points) -> Object;
+		/**
+		 * @brief Create an Object representing a quad
+		 *
+		 * @param position The position of the quad
+		 * @param width The width of the quad
+		 * @param height The height of the quad
+		 *
+		 * @return The created Object
+		 */
 		[[nodiscard]] auto CreateQuad(glm::vec3 position, F32 width, F32 height) -> Object;
 	}
 }
