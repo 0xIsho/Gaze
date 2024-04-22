b966fbfa614600c44076b21c883330eb8afbd823
Isho Antar <0xisho@proton.me>
2024-04-22 16:20:29 +0200

    Events: Document the public API

--- lib/Events/include/Events/KeyEvent.hpp
+++ lib/Events/include/Events/KeyEvent.hpp
@@ -5,17 +5,42 @@
 #include "Input/KeyCode.hpp"
 
 namespace Gaze::Events {
+	/**
+	 * @brief The Key class represents a key event.
+	 */
 	class Key : public Event
 	{
 	public:
+		/**
+		 * @brief The Key constructor.
+		 *
+		 * @param key The keycode.
+		 * @param scancode The scancode.
+		 * @param mods The mods.
+		 */
 		Key(Input::Key key, int scancode, Input::Mod mods) noexcept
 			: m_Key(key)
 			, m_Scancode(scancode)
 			, m_Mods(mods)
 		{}
 
+		/*
+		 * @brief Returns the keycode.
+		 *
+		 * @return The keycode.
+		 */
 		[[nodiscard]] auto Keycode()  const noexcept { return m_Key; }
+		/**
+		 * @brief Returns the scancode.
+		 *
+		 * @return The scancode.
+		 */
 		[[nodiscard]] auto Scancode() const noexcept { return m_Scancode; }
+		/**
+		 * @brief Returns the mods.
+		 *
+		 * @return The mods.
+		 */
 		[[nodiscard]] auto Mods()     const noexcept { return m_Mods; }
 
 	private:
@@ -24,6 +49,9 @@ namespace Gaze::Events {
 		Input::Mod m_Mods;
 	};
 
+	/*
+	 * @brief The KeyPressed class represents a key pressed event.
+	 */
 	class KeyPressed : public Key
 	{
 	public:
@@ -35,6 +63,11 @@ namespace Gaze::Events {
 		{}
 	};
 
+	/*
+	 * @brief The KeyRepeat class represents a key repeat event.
+	 *
+	 * This event is triggered when a key is held down.
+	 */
 	class KeyRepeat : public Key
 	{
 	public:
@@ -46,6 +79,9 @@ namespace Gaze::Events {
 		{}
 	};
 
+	/*
+	 * @brief The KeyReleased class represents a key released event.
+	 */
 	class KeyReleased : public Key
 	{
 	public:
@@ -59,6 +95,13 @@ namespace Gaze::Events {
 }
 
 namespace Gaze {
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::Key& event) -> std::string
 	{
@@ -86,18 +129,39 @@ namespace Gaze {
 		);
 	}
 
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::KeyPressed& event) -> std::string
 	{
 		return std::format("{}\n  - State: Pressed", ToString(static_cast<Events::Key>(event)));
 	}
 
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::KeyRepeat& event) -> std::string
 	{
 		return std::format("{}\n  - State: Repeat", ToString(static_cast<Events::Key>(event)));
 	}
 
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::KeyReleased& event) -> std::string
 	{
--- lib/Events/include/Events/MouseEvent.hpp
+++ lib/Events/include/Events/MouseEvent.hpp
@@ -5,58 +5,116 @@
 #include "Input/KeyCode.hpp"
 
 namespace Gaze::Events {
+	/**
+	 * @brief Represents a mouse move event
+	 *
+	 * This event is triggered when the cursor is moved around in the window.
+	 */
 	class MouseMove : public Event
 	{
 	public:
 		GAZE_REGISTER_EVENT(MouseMove)
 
 	public:
+		/**
+		 * @brief Constructs a new MouseMove event.
+		 *
+		 * @param x The x coordinate of the cursor.
+		 * @param y The y coordinate of the cursor.
+		 */
 		MouseMove(double x, double y) noexcept
 			: m_X(x)
 			, m_Y(y)
 		{}
 
+		/**
+		 * @brief Returns the x coordinate of the cursor.
+		 *
+		 * @return The x coordinate of the cursor.
+		 */
 		[[nodiscard]] auto X() const noexcept { return m_X; }
+		/**
+		 * @brief Returns the y coordinate of the cursor.
+		 *
+		 * @return The y coordinate of the cursor.
+		 */
 		[[nodiscard]] auto Y() const noexcept { return m_Y; }
 
 	private:
-		double m_X;
-		double m_Y;
+		double m_X; /**< The x coordinate of the cursor.  */
+		double m_Y; /**< The y coordinate of the cursor.  */
 	};
 
+	/**
+	 * @brief Represents a mouse button state change event
+	 */
 	class MouseButton : public Event
 	{
 	public:
+		/**
+		 * @brief Constructs a new MouseButton event.
+		 *
+		 * @param button The button that was clicked.
+		 * @param mods The modifiers that were pressed when the button was clicked. 
+		 */
 		MouseButton(Input::MouseButton button, Input::Mod mods) noexcept
 			: m_Button(button)
 			, m_Mods(mods)
 		{}
 
+		/**
+		 * @brief Returns the button that was clicked.
+		 *
+		 * @return The button that was clicked.
+		 */
 		[[nodiscard]] auto Button() const noexcept { return m_Button; }
-		[[nodiscard]] auto Mods()   const noexcept { return m_Mods; }
+		/**
+		 * @brief Returns the modifiers that were pressed when the button was clicked.
+		 *
+		 * @return The modifiers that were pressed when the button was clicked.
+		 */
+		[[nodiscard]] auto Mods() const noexcept { return m_Mods; }
 
 	private:
-		Input::MouseButton m_Button;
-		Input::Mod         m_Mods;
+		Input::MouseButton m_Button; /**< The button that was clicked. */
+		Input::Mod         m_Mods;   /**< The modifiers that were pressed when the button was clicked. */
 	};
 
+	/**
+	 * @brief Represents a mouse click event
+	 */
 	class MouseButtonPressed : public MouseButton
 	{
 	public:
 		GAZE_REGISTER_EVENT(MouseButtonPressed);
 
 	public:
+		/**
+		 * @brief Constructs a new MouseButtonPressed event.
+		 *
+		 * @param button The button that was clicked.
+		 * @param mods The modifiers that were pressed when the button was clicked.
+		 */
 		MouseButtonPressed(Input::MouseButton button, Input::Mod mods) noexcept
 			: MouseButton(button, mods)
 		{}
 	};
 
+	/**
+	 * @brief Represents a mouse release event
+	 */
 	class MouseButtonReleased : public MouseButton
 	{
 	public:
 		GAZE_REGISTER_EVENT(MouseButtonReleased);
 
 	public:
+		/**
+		 * @brief Constructs a new MouseButtonReleased event.
+		 *
+		 * @param button The button that was released.
+		 * @param mods The modifiers that were pressed when the button was released.
+		 */
 		MouseButtonReleased(Input::MouseButton button, Input::Mod mods) noexcept
 			: MouseButton(button, mods)
 		{}
@@ -64,6 +122,13 @@ namespace Gaze::Events {
 }
 
 namespace Gaze {
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::MouseMove& event) -> std::string
 	{
@@ -78,6 +143,13 @@ namespace Gaze {
 		);
 	}
 
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::MouseButton& event) -> std::string
 	{
@@ -115,12 +187,26 @@ namespace Gaze {
 		);
 	}
 
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::MouseButtonPressed& event) -> std::string
 	{
 		return std::format("{}\n  - State: Pressed", ToString(static_cast<Events::MouseButton>(event)));
 	}
 
+	/**
+	 * @brief Returns the string representation of the event.
+	 *
+	 * @param event The event.
+	 *
+	 * @return The string representation of the event.
+	 */
 	[[nodiscard]]
 	inline auto ToString(const Events::MouseButtonReleased& event) -> std::string
 	{
