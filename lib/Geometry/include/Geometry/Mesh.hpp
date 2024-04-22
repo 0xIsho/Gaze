5b43530f065e33b1fb2441c0bca5d14dabce99c6
Isho Antar <0xisho@proton.me>
2024-04-22 16:38:09 +0200

    Geometry: Document the public API

--- lib/Geometry/include/Geometry/Mesh.hpp
+++ lib/Geometry/include/Geometry/Mesh.hpp
@@ -6,36 +6,79 @@
 #include <initializer_list>
 
 namespace Gaze::Geometry {
+	/**
+	 * @brief Represents a vertex in a mesh.
+	 */
 	struct Vertex
 	{
 		float x, y, z;    /**< Coordinates */
 		float nx, ny, nz; /**< Normals */
 	};
 
+	/**
+	 * @brief Represents an index in a mesh.
+	 */
 	using Index = U32;
 
+	/**
+	 * @brief Represents a primitive in a mesh.
+	 */
 	struct Primitive
 	{
-		std::vector<Vertex> vertices;
-		std::vector<Index>  indices;
+		std::vector<Vertex> vertices; /**< List of vertices in the primitive */
+		std::vector<Index>  indices;  /**< List of indices in the primitive */
 	};
 
+	/**
+	 * @brief Represents a geometric mesh.
+	 */
 	class Mesh
 	{
 	public:
+		/**
+		 * @brief Size of a vertex in bytes.
+		 */
 		static constexpr auto kVertexSize = sizeof(Vertex);
+		/**
+		 * @brief Size of an index in bytes.
+		 */
 		static constexpr auto kIndexSize  = sizeof(Index);
 
 	public:
-		Mesh(std::initializer_list<Vertex> vertices, std::initializer_list<Index> indices);
-		Mesh(std::vector<Vertex> vertices, std::vector<Index> indices);
+		/**
+		 * @brief Constructs a mesh from a list of primitives.
+		 *
+		 * @param primitives List of primitives.
+		 */
 		Mesh(std::initializer_list<Primitive> primitives);
+		/**
+		 * @brief Constructs a mesh from a list of primitives.
+		 *
+		 * @param primitives List of primitives.
+		 */
 		Mesh(std::vector<Primitive> primitives);
+		/**
+		 * @brief Constructs a mesh from lists of vertices and indices.
+		 *
+		 * @param vertices List of vertices.
+		 * @param indices List of indices.
+		 */
+		Mesh(std::vector<Vertex> vertices, std::vector<Index> indices);
+		/**
+		 * @brief Constructs a mesh from lists of vertices and indices.
+		 *
+		 * @param vertices List of vertices.
+		 * @param indices List of indices.
+		 */
+		Mesh(std::initializer_list<Vertex> vertices, std::initializer_list<Index> indices);
 
+		/**
+		 * @brief Returns the list of primitives in the mesh.
+		 */
 		[[nodiscard]] auto Primitives() const noexcept -> const std::vector<Primitive>&;
 
 	private:
-		std::vector<Primitive> m_Primitives;
+		std::vector<Primitive> m_Primitives; /**< List of primitives in the mesh */
 	};
 
 	inline auto Mesh::Primitives() const noexcept -> const std::vector<Primitive>&
