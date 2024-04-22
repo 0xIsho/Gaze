3b6f1a844f2801576805bbf92256acc58ed8d7f9
Isho Antar <0xisho@proton.me>
2024-04-22 14:12:42 +0200

    Config: Document the public API

--- lib/Config/include/Config/Configuration.hpp
+++ lib/Config/include/Config/Configuration.hpp
@@ -7,9 +7,35 @@
 #include <unordered_map>
 
 namespace Gaze::Config {
+	/**
+	 * @brief A generic converter that converts a string to a specified type.
+	 *
+	 * This struct acts as a callable object that attempts to convert
+	 * a given string to a given type.
+	 *
+	 * This template should be specialzed for specific types.
+	 *
+	 * @tparam T The type to which the string should be converted.
+	 */
 	template<typename T>
 	struct Converter
 	{
+		/**
+		 * @brief Converts a string to the specified type.
+		 *
+		 * This function attempts to convert the given string to the specified
+		 * type and assigns the result to the provided output parameter.
+		 *
+		 * Note that this operator attemps an implicit conversion which is
+		 * likely *not* what you want.
+		 *
+		 * Speicialize this template for specific types.
+		 * 
+		 * @param str The string to be converted.
+		 * @param out The output parameter where the result will be stored.
+		 *
+		 * @return true if the conversion was successful, false otherwise.
+		 */
 		auto operator()(const std::string& str, T& out) const -> bool
 		{
 			out = str;
@@ -18,9 +44,26 @@ namespace Gaze::Config {
 		}
 	};
 
+	/**
+	 * @brief Specialization of the generic converter for converting strings to int.
+	 *
+	 * This struct provides a specialized `operator()` for converting a given string to an int
+	 * and assigning the result to the provided output parameter.
+	 */
 	template<>
 	struct Converter<int>
 	{
+		/**
+		 * @brief Converts a string to an int.
+		 *
+		 * This function attempts to convert the given string to an int and assigns
+		 * the result to the provided output parameter.
+		 *
+		 * @param str The string to be converted.
+		 * @param out The output parameter where the result will be stored.
+		 *
+		 * @return true if the conversion was successful, false otherwise.
+		 */
 		auto operator()(const std::string& str, int& out) const -> bool
 		{
 			errno = 0; // strtol() modifies it in case of errors
@@ -48,14 +91,52 @@ namespace Gaze::Config {
 		}
 	};
 
+	/**
+	 * @brief A configuration section. Maps variables to their respective values
+	 *
+	 * @see @ref Configuration
+	 */
 	class Section
 	{
 	public:
 		using Map = std::unordered_map<std::string, std::string>;
 
 	public:
+		/**
+		 * @brief Add a new Key-Value pair to the section
+		 */
 		auto Add(std::string name, std::string value) -> void;
 
+		/**
+		 * @brief Retieve a value from the section
+		 *
+		 * This function retrieves the value of the given variable from the section.
+		 * The output parameter is *not* modified if the function fails.
+		 *
+		 * Example usage:
+		 * 
+		 * @code
+		 * int x = 0;
+		 * if (mySection.Get<int>("foo", x)) {
+		 *   // Do something with x
+		 * }
+		 * else {
+		 *   // Error! Either variable "foo" doesn't exist or its value could not be
+		 *   // converted to an 'int'
+		 * }
+		 * @endcode
+		 * 
+		 * @tparam T The type of the stored value
+		 * @tparam TConverter The @ref Converter instance to use for the string -> T conversion
+		 *
+		 * @param name The name of the variable
+		 * @param out The output parameter in which the value will be stored
+		 *
+		 * @return true If the variable exists and was successfully converted to type T
+		 *         false otherwise.
+		 *
+		 * @note Parameter 'out' is not modified on failure
+		 */
 		template<typename T, Converter TConverter = Converter<T>()>
 		[[nodiscard]] auto Get(const std::string& name, T& out) -> bool
 		{
@@ -66,6 +147,9 @@ namespace Gaze::Config {
 			return TConverter(m_ValuesMap[name], out);
 		}
 
+		/**
+		 * @brief Check if the section is empty
+		 */
 		[[nodiscard]] auto IsEmpty() const -> bool
 		{
 			return m_ValuesMap.empty();
@@ -75,14 +159,72 @@ namespace Gaze::Config {
 		std::unordered_map<std::string, std::string> m_ValuesMap;
 	};
 
+	/**
+	 * @brief A configuration object. Holds a collection of configuration sections
+	 *
+	 * Example configuration:
+	 *
+	 * @code
+	 * [section1]
+	 * foo = bar
+	 *
+	 * [section2]
+	 * baz = qux
+	 * @endcode
+	 * 
+	 * @see @ref Section
+	 */
 	class Configuration
 	{
 	public:
 		using Map = std::unordered_map<std::string, Section>;
 
 	public:
+		/**
+		 * @brief Add a new section to the configuration
+		 *
+		 * This function adds a new section to the configuration.
+		 *
+		 * @param path The path of the section. Freeform string for now. No special handling takes place
+		 * @param section The section to add. Populate a section object then pass it to this function
+		 */
 		auto Add(std::string path, Section section) -> void;
 
+		/**
+		 * @brief Retrieve a value from the configuration object
+		 *
+		 * This function, similar to @ref Section::Get() retrieves a value from the
+		 * configuration object.
+		 *
+		 * Example usage:
+		 *
+		 * Assuming the following configuration block:
+		 * @code
+		 * [/Path/To/Section]
+		 * Foo = 10
+		 * @endcode
+		 * 
+		 * @code
+		 * int x = 0;
+		 * if (myConfig.Get<int>("/Path/To/Section", "Foo", x)) {
+		 *   // Success! x = 10
+		 * }
+		 * else {
+		 *   // Error! Variable doesn't exist or could not be converted to an 'int'
+		 * }
+		 * @endcode
+		 *
+		 * @tparam T The type of the configuration variable's value
+		 * @tparam TConverter The @ref Converter instance used to do the string -> T conversion
+		 *
+		 * @param path Section's path, the part in [square brackets] in the example above
+		 * @param name The variable's name
+		 *
+		 * @return true If the variable exists and was successfully converted to 'T'
+		 *         false otherwise.
+		 *
+		 * @note The output parameter is not modified in case of failure.
+		 */
 		template<typename T, Converter TConverter = Converter<T>()>
 		[[nodiscard]] auto Get(const std::string& path, const std::string& name, T& out) -> bool
 		{
--- lib/Config/include/Config/Parser.hpp
+++ lib/Config/include/Config/Parser.hpp
@@ -7,6 +7,23 @@ namespace Gaze::Config {
 	class Section;
 	class Configuration;
 
+	/**
+	* @brief This class is used to parse a configuration file.
+	*
+	* The configuration file is expected to be in the following format:
+	*
+	* [section]
+	* parameter = value
+	* 
+	* "section" and "parameter" are case-sensitive. "section" and "value" are
+	* optional and may be omited. The default section, if omited, is '/'.
+	*
+	* The "section" is optional. If the "section" is not specified, the "parameter" will be added to the "default" section.
+	*
+	* The "parameter" is optional. If the "parameter" is not specified, the "value" will be ignored.
+	*
+	* The "value" is optional. If the "value" is not specified, the "parameter" will be set to an empty string.
+	*/
 	class Parser
 	{
 	public:
@@ -14,12 +31,38 @@ namespace Gaze::Config {
 		struct Impl;
 
 	public:
+		/**
+		 * @brief Construct a Parser object
+		 *
+		 * @param outConfig Output parameter where parsed configuration will be
+		 *   stored. MUST be a valid pointer!
+		 */
 		Parser(Configuration* outConfig);
+
+		/**
+		 * @brief Destroy the parser object
+		 */
 		~Parser();
 
 		// "path" relative to "s_ConfigDirPath"
+		/**
+		 * @brief Load/Parse a configuration file
+		 *
+		 * @param path The path to the configuration file; relative to "ConfigDirectoryPath"
+		 *
+		 * @return true If the parsing was successful, false otherwise
+		 *
+		 * @note The output configuration parameter (provided in the constructor) is
+		 *   not modified in case of failure.
+		 */
 		[[nodiscard]] auto Load(const Path& path) -> bool;
 
+		/**
+		 * @brief Set the root of the configuration directory
+		 *
+		 * Configuration file lookups, in @ref Load() for example, are relative to
+		 * the path provided here. Defaults to the executables current working directory.
+		 */
 		static auto ConfigDirectoryPath(const Path& path) -> void;
 
 	private:
